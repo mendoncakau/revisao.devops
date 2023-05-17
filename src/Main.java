ee4d36c90275a23c0ecc53b974df00d268e35848
Kaue <mendoncakau@gmail.com>
2023-05-17 14:39:12 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello world!");
+        System.out.println("Informe seu perfil");
+        String texto = sc.nextLine();
+
+
     }
 }
